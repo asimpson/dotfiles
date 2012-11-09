9f50707b4d3ae9d95477e4f5ecc3c9919d4bfcc6
Adam Simpson <adam@heysparkbox.com>
2012-11-09 13:00:14 -0500

    mamp vhost script, watch/reload script

--- osx/mamp_vh.sh
+++ osx/mamp_vh.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
-# Please create a folder called "vhost" at /Applications/MAMP/Library/
-# Add " Include vhosts/* " to the end of the httpd.conf file located at /Applications/MAMP/conf/apache/httpd.conf
+# Add " Include vhosts/httpd-vhosts.conf " to the end of the httpd.conf file located at /Applications/MAMP/conf/apache/httpd.conf
 
-# Ask for site name
+mkdir -p /Applications/MAMP/Library/vhosts;
+mkdir -p /Applications/MAMP/Library/vhosts/domains;
+
 if [ "$1" = "create" ] || [ "$1" = "add" ]; then
-  echo "Enter the site name (eg Test Site):";
-  read projectName;
-
-  # Ask for document root
+ # Ask for document root
   echo "Enter the document root (relative to 'htdocs'):";
   read documentRoot;
 
   # Ask for domain name
-  echo "Enter local domain: (eg. local.com)";
+  echo "Enter local domain: (eg. local.com):";
   read domain;
 
+   # Ask for domain name
+  echo "MAMP Port Nubmer";
+  read port;
+
   # Add vhost
-  touch /Applications/MAMP/Library/vhosts/$documentRoot;
+  touch /Applications/MAMP/Library/vhosts/domains/$domain;
 
-  echo "
-  ServerName $domain
-  DocumentRoot /Applications/MAMP/htdocs/$documentRoot
-    Options FollowSymLinks
-    Options Indexes FollowSymLinks MultiViews
-  ErrorLog /Applications/MAMP/logs/$domain.error.log" >> /Applications/MAMP/Library/vhosts/$domain;
+  echo "<VirtualHost *:$port>
+    DocumentRoot "/Applications/MAMP/htdocs/$documentRoot"
+    ServerName $domain
+    <Directory "/Applications/MAMP/htdocs/$documentRoot">
+        Options All
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+    </Directory>
+</VirtualHost>" >> /Applications/MAMP/Library/vhosts/domains/$domain;
 
-  echo "
-  127.0.0.1 $domain" >> /etc/hosts;
+  echo "127.0.0.1 $domain" >> /etc/hosts;
 
   # Restart MAMP
   /Applications/MAMP/bin/apache2/bin/apachectl restart;
 
-  echo "Visit $domain:port/ to view your site";
+  echo "Visit $domain:port to view your site";
 fi
 
 if [ "$1" = "remove" ] || [ "$1" = "delete" ]; then
-    echo "Enter the site name you wish to remove"
+    echo "Here are the current custom local domains:"
+    for file in /Applications/MAMP/Library/vhosts/domains/*
+    do
+      if [ -f "$file" ];then
+       echo ${file##/*/}
+      fi
+    done
+    echo "Enter the site name you wish to remove:"
     read siteName;
 
     sed -i.bak "/$siteName/d" /etc/hosts;
-    rm /Applications/MAMP/Library/vhosts/$siteName;
+    rm /Applications/MAMP/Library/vhosts/domains/$siteName;
 fi
