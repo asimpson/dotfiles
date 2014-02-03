f6eee41c8605611e6f15383b56d5a88fc1578c9f
Adam Simpson <adam@heysparkbox.com>
2014-02-03 15:30:20 -0500

    fix: change condition for cleanDesktop

--- launch-scripts/cleanDesktop.sh
+++ launch-scripts/cleanDesktop.sh
@@ -3,7 +3,7 @@ function cleanDesktop {
   shopt -s nullglob
   for file in /Users/asimpson/Desktop/*
   do
-    if [ "$file" == "/Users/asimpson/Desktop/skype-attachments" -o "$file" == "/Users/asimpson/Desktop/old-downloads"];then
+    if [[ "$file" == "/Users/asimpson/Desktop/skype-attachments" || "$file" == "/Users/asimpson/Desktop/old-downloads" ]]; then
       echo "don't move these";
     else
       if [[ -n $(find "$file" -amin +60) ]]; then
