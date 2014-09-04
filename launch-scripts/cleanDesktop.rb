24c7502d593be527239d23cd6f818274f85b6c12
Adam Simpson <adam@heysparkbox.com>
2014-09-04 10:17:22 -0400

    fix: tweak clean desktop ruby script

--- launch-scripts/cleanDesktop.rb
+++ launch-scripts/cleanDesktop.rb
@@ -3,18 +3,17 @@ require 'date'
 require 'fileutils'
  
 desktop = "/Users/asimpson/Desktop/"
-oldDownloads = "/Users/asimpson/Desktop/old-downloads/"
 dest = "/Users/asimpson/Inbox/"
  
 Dir.foreach(desktop) do |file|
-  next if File.fnmatch?('.*', file, File::FNM_DOTMATCH)
+  next if File.fnmatch?('.*', file, File::FNM_DOTMATCH) or file == 'old-downloads'
  
   Dir.chdir(desktop)
   fileTime = File.atime(file)
   timeNow = Time.now
  
   if fileTime < (timeNow - 14400)
-    filePath = '/Users/arthursimpson/Desktop/#{file}'
+    filePath = "#{desktop}#{file}"
     `/usr/local/bin/tag -a inbox '#{file}'`
     FileUtils.mv(file, dest, {:verbose => true})
   end
