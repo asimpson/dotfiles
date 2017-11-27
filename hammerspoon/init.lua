9420ba3bd5143d1107757a290a4e095c7c2fb8d9
Adam Simpson <adam@heysparkbox.com>
2017-11-27 15:04:02 -0500

    feat: :hammer: add hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,77 @@
+choices =  {
+   {
+      ['text'] = 'Toggle Zoom',
+      ['func'] = 'toggleZoomMute'},
+   {
+      ['text'] = 'Toggle Caffeine',
+      ['func'] = 'toggleCaffine'
+   },
+   {
+      ['text'] = 'Org capture',
+      ['func'] = 'capture'
+   },
+   {
+      ['text'] = 'Emacs frame',
+      ['func'] = 'frame'
+   }
+}
+
+function frame()
+   os.execute("/usr/local/bin/emacsclient -c -n &")
+end
+
+function capture()
+   os.execute("/usr/local/bin/emacsclient -c -n -e '(org-capture nil \"b\")' &")
+   hs.timer.doAfter(1, function() hs.application.find('Emacs'):findWindow("Capture"):focus() end)
+end
+
+function toggleZoomMute()
+    hs.application.launchOrFocus("zoom.us")
+    local zoom = hs.appfinder.appFromName("zoom.us")
+
+    local muteAudio = {"Meeting", "Mute Audio"}
+    local unMuteAudio = {"Meeting", "Unmute Audio"}
+    -- close the extra zoom window
+    local close = {"Window", "Close"}
+    local free = {"Window", "Zoom - Free Account"}
+
+    local mute = zoom:findMenuItem(muteAudio)
+    local unmute = zoom:findMenuItem(unMuteAudio)
+    local freeWindow = zoom:findMenuItem(free)
+
+    if (freeWindow) then
+        zoom:selectMenuItem(free)
+        zoom:selectMenuItem(close)
+    end
+    if (mute) then
+        zoom:selectMenuItem(muteAudio)
+        hs.alert.show("Muted")
+    end
+    if (unmute) then
+        zoom:selectMenuItem(unMuteAudio)
+        hs.alert.show("Unmuted")
+    end
+end
+
+function toggleCaffine()
+   local state = hs.caffeinate.toggle('displayIdle')
+   if (state) then
+    hs.alert.show('caffeinated')
+   else
+    hs.alert.show('decaffeinated')
+   end
+end
+
+function chooser()
+   local choose = hs.chooser.new(function(selection)
+         if (selection) then
+          _G[selection['func']]()
+         end
+   end)
+   choose:choices(choices)
+   choose:rows(3)
+   choose:width(20)
+   choose:show()
+end
+
+hs.hotkey.bind({"cmd, shift"}, ".", chooser)
