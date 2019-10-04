bfcf6208608dd483b0eb49599071a9874050ba46
Adam Simpson <adam@heysparkbox.com>
2019-10-03 23:55:50 -0400

    feat: update borg backup file

--- backup.rb
+++ backup.rb
@@ -1,63 +1,27 @@
 #!/usr/bin/env ruby
-$prefix = 'com.apple.TimeMachine.'
 $host = 'rsync'
 $repo = 'laptop'
-# Where the snapshot will be mounted.
-$mount = "#{Dir.home}/borg"
-$ref_file = "#{Dir.home}/.borg-snapshot"
 # Define exclusions here.
 # https://borgbackup.readthedocs.io/en/latest/usage/help.html#borg-help-patterns
 $exclude_file = "#{Dir.home}/.borg-exclude.txt"
 # This is a gpg encrypted file that contains the repo password.
 $password_file = "#{Dir.home}/.borg-rsync-pass.gpg"
 
-def record_snapshot snapshot
-  File.new($ref_file, "w")
-  File.open($ref_file, "w") { |f| f.write snapshot }
-end
-
-def cleanup
-  puts "Cleaning up after the borg..."
-  ref = File.read($ref_file)
-  `diskutil umount #{$mount}`
-  `tmutil deletelocalsnapshots #{ref}`
-  File.delete($ref_file)
-end
-
 def run_borg
   dotfiles = Dir.glob(File.expand_path('~/.*'))
-               .map { |file| "#{$mount}#{file}"}
                .select { |f| f[-1] != "." }
                .join " "
-  library = "#{$mount}/Users/asimpson/Library"
-  projects = "#{$mount}/Users/asimpson/Projects"
+  projects = "/home/asimpson/Projects"
+  etc = "/etc"
 
   ENV['BORG_PASSCOMMAND'] = "gpg -dq #{$password_file}"
   puts "breaking lock if there is one..."
   `borg break-lock --remote-path=borg1 #{$host}:#{$repo}`
   puts "starting borg create..."
-  `borg create --remote-path=borg1 -s --exclude-from #{$exclude_file} #{$host}:#{$repo}/::{now} #{dotfiles} #{library} #{projects}`
+  `borg create --remote-path=borg1 -s --exclude-from #{$exclude_file} #{$host}:#{$repo}/::{now} #{dotfiles} #{etc} #{projects}`
   puts "pruning..."
   `borg prune --remote-path=borg1 -s --keep-daily 10 --keep-monthly 6 #{$host}:#{$repo}`
 end
 
-def new
-  snapshot = `tmutil localsnapshot`.split(':')[1].strip
-  `diskutil umount #{$mount}`
-  `mount_apfs -s #{$prefix}#{snapshot} / #{$mount}`
-  record_snapshot snapshot
-end
-
-existing_snapshot_file = File.file?($ref_file) && File.read($ref_file)
-local_snapshots = !`tmutil listlocalsnapshots /`.empty?
-
-if existing_snapshot_file and local_snapshots then
-  puts "Found existing snapshot. Resuming..."
-  `mount_apfs -s #{$prefix}#{existing_snapshot_file} / #{$mount}`
-else
-  puts "Starting new backup..."
-  new
-end
-
+puts "Starting new backup..."
 run_borg
-cleanup
