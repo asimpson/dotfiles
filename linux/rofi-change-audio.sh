e4cee6f67e2b564775f873c7620dd1df0bd27b1a
Adam Simpson <adam@heysparkbox.com>
2020-05-27 11:16:10 -0400

    fix: rofi change audio script fix all playing inputs

--- linux/rofi-change-audio.sh
+++ linux/rofi-change-audio.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source=$(pactl list short sinks | cut -f 2 | rofi -dpi 1 -dmenu -p 'Change audio:');
-input=$(pactl list sink-inputs short | cut -f 1)
-pactl move-sink-input "$input" "$source";
+source="$(pactl list short sinks | cut -f 2 | rofi -dpi 1 -dmenu -p "Change audio:")";
+inputs="$(pactl list sink-inputs short | cut -f 1)";
+
+for input in $inputs; do
+  pactl move-sink-input "$input" "$source";
+done
+
 pactl set-default-sink "$source";
