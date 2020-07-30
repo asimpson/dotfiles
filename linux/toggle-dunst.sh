a793d7fb5ac2590dd9057c5c88bac6d5781f93f8
Adam Simpson <adam@heysparkbox.com>
2020-07-29 20:31:34 -0400

    feat: add dunst toggle script for rofi

---
+++ linux/toggle-dunst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+if [[ -f "/tmp/dunst-muted" ]]; then
+  notify-send "DUNST_COMMAND_RESUME";
+  rm /tmp/dunst-muted;
+else
+  notify-send "DUNST_COMMAND_PAUSE";
+  touch /tmp/dunst-muted;
+fi
