40c675f0099f06f5cc8634f75f09fa40b963cee4
Adam Simpson <adam@adamsimpson.net>
2021-08-27 10:32:11 -0400

    fix: toggle-dunst now uses dunstctl instead of tmp hacks

--- linux/toggle-dunst.sh
+++ linux/toggle-dunst.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-
-if [ -f "/tmp/dunst-muted" ]; then
-  notify-send "DUNST_COMMAND_RESUME";
-  rm /tmp/dunst-muted;
-else
-  notify-send "DUNST_COMMAND_PAUSE";
-  touch /tmp/dunst-muted;
-fi
+dunstctl set-paused toggle
