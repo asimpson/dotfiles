d1799f8476206b14f6a86e167d5714c84c6162e9
Adam Simpson <adam@adamsimpson.net>
2025-02-25 09:56:37 -0500

    fix: gen-env broke path

--- linux/gen-env.sh
+++ linux/gen-env.sh
@@ -2,14 +2,20 @@
 
 echo 'eval "$(lorri direnv)"' >> .envrc
 echo 'use nix' >> .envrc
+echo 'mkdir -p $TMPDIR' >> .envrc # https://github.com/direnv/direnv/issues/1345
 
 cat > shell.nix<< NIX
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [];
+  buildInputs = [
+    pkgs.go
+    pkgs.gopls
+  ];
 
   shellHook = ''
+    export GOPATH=$PWD/.go
+    export PATH=$PWD/.go/bin:$PATH
   '';
 }
 NIX
